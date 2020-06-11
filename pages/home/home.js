8b819bc9de027f914bde871956e9443ace1656b3
zhangke <1826213722@qq.com>
2020-06-11 13:05:18 +0800

    广场界面完成（除了动态（之后做成组件））

--- pages/home/home.js
+++ pages/home/home.js
@@ -34,9 +34,13 @@ Page({
     // 应援榜
     rankinglist: [
       {
+        // 头像地址
         'imgUrl': 'https://wx1.sinaimg.cn/mw690/005FbhyYgy1gfnlk8eh56j306y06yglo.jpg',
+        // 头像边框颜色
         'headcolor': '#ffcb09',
+        // 排名图标
         'rankimg': '/images/home/one.png',
+        // 成员名称
         'memberName': 'SNH48-孙芮'
       },
       {
@@ -50,6 +54,55 @@ Page({
         'headcolor': '#f0a639',
         'rankimg': '/images/home/three.png',
         'memberName': 'SNH48-莫寒'
+      }
+    ],
+    // 正在直播的成员
+    livinglist: [
+      {
+        // 头像地址
+        'imgUrl': 'https://wx1.sinaimg.cn/mw690/005FbhyYgy1gfnlk8eh56j306y06yglo.jpg',
+        // 头像边框颜色（目前来看是一个颜色）
+        // 'headcolor': '#ffcb09',
+        // 成员名称
+        'memberName': 'SNH48-孙芮',
+        // 队名
+        'team': 'SII',
+        // 队色
+        'teamColor': '#87ceeb',
+        // 直播标题
+        'liveTitle': '是我是我是我'
+      },
+      {
+        'imgUrl': 'https://wx3.sinaimg.cn/mw690/005FbhyYgy1gfnlkq1ubmj305706ydfx.jpg',
+        // 'headcolor': '#bebebe',
+        'memberName': 'SNH48-宋昕冉',
+        'team': 'X',
+        'teamColor': '#a8cd25',
+        'liveTitle': '哈'
+      },
+      {
+        'imgUrl': 'https://wx2.sinaimg.cn/mw690/005FbhyYgy1gfnlkvwqk9j306y06yq2u.jpg',
+        // 'headcolor': '#f0a639',
+        'memberName': 'SNH48-莫寒',
+        'team': 'SII',
+        'teamColor': '#87ceeb',
+        'liveTitle': '(  •̆ ᵕ •̆ )◞♡ '
+      },
+      {
+        'imgUrl': 'https://wx3.sinaimg.cn/mw690/005FbhyYly1gfkl3olgp6j306y06ywed.jpg',
+        // 'headcolor': '#bebebe',
+        'memberName': 'BEJ48-段艺璇',
+        'team': 'B',
+        'teamColor': '#e6356c',
+        'liveTitle': '纠结发色'
+      },
+      {
+        'imgUrl': 'https://wx3.sinaimg.cn/mw690/005FbhyYgy1gfo8wxiz8qj306y04mq2w.jpg',
+        // 'headcolor': '#f0a639',
+        'memberName': 'GNZ48-卢静',
+        'team': 'NII',
+        'teamColor': '#ffd801',
+        'liveTitle': '唠唠 '
       }
     ]
   },
